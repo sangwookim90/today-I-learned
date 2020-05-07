35de69e48b3474997cac3136fee83ecdbf3c9a96
Sangwoo Kim <sangwookim90@gmail.com>
2020-05-08 02:00:54 +0900

    2020/5/8 02:00 - 9강까지 수강완료

---
+++ studyolle/src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,58 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    // 연관관계가 복잡해질 때 EqualsAndHashCode에서 서로 다른 연관관계를 순환참조하여 무한루프, stackoverflow 발생하므로 id만
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    // 255보다 더 커질 수 있으므로 Lob 사용
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+
+
+
+}
